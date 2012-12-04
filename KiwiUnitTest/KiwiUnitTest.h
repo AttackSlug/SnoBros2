c11bdf40abc67a08ff7ad45bfdb02dc70252117a
Chad Jablonski <chad@dinocore.net>
2012-12-04 12:05:53 -0500

    Create KiwiUnitTest bundle

---
+++ KiwiUnitTest/KiwiUnitTest.h
@@ -0,0 +1,13 @@
+//
+//  KiwiUnitTest.h
+//  KiwiUnitTest
+//
+//  Created by Cjab on 12/4/12.
+//  Copyright (c) 2012 Attack Slug. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KiwiUnitTest : SenTestCase
+
+@end
---
+++ KiwiUnitTest/KiwiUnitTest.m
@@ -0,0 +1,32 @@
+//
+//  KiwiUnitTest.m
+//  KiwiUnitTest
+//
+//  Created by Cjab on 12/4/12.
+//  Copyright (c) 2012 Attack Slug. All rights reserved.
+//
+
+#import "KiwiUnitTest.h"
+
+@implementation KiwiUnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KiwiUnitTest");
+}
+
+@end
