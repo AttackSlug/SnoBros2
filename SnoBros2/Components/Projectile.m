9ce4f7ce9ba4d156a20cadd306f40c0abb05474b
Chad Jablonski <chad@dinocore.net>
2012-12-15 17:08:42 -0500

    Move projectile collision to projectile system

--- SnoBros2/Components/Projectile.m
+++ SnoBros2/Components/Projectile.m
@@ -15,14 +15,11 @@
 
 @implementation Projectile
 
+@synthesize damage = damage_;
+
 - (id)initWithEntity:(Entity *)entity {
   self = [super initWithEntity:entity];
   if (self) {
-    NSString *name = [entity_.uuid stringByAppendingString:@"|collidedWith"];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(collidedWith:)
-                                                 name:name
-                                               object:nil];
   }
   return self;
 }
@@ -49,23 +46,6 @@
   [[NSNotificationCenter defaultCenter] postNotificationName:@"walkTo"
                                                       object:self
                                                     userInfo:walkToData];
-}
-
-
-
-- (void)collidedWith:(NSNotification *)notification {
-  Entity *otherEntity  = [notification userInfo][@"otherEntity"];
-
-  NSValue  *damageVal  = [NSValue value:&damage_ withObjCType:@encode(int *)];
-  NSDictionary *damageData = @{@"amount": damageVal, @"entity": otherEntity};
-  [[NSNotificationCenter defaultCenter] postNotificationName:@"takeDamage"
-                                                      object:self
-                                                    userInfo:damageData];
-
-  NSDictionary *destroyData = @{@"entity": entity_};
-  [[NSNotificationCenter defaultCenter] postNotificationName:@"destroyEntity"
-                                                      object:self
-                                                    userInfo:destroyData];
 }
 
 @end
